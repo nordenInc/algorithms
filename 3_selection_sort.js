f0d30f889d5d719770af34d201db87c9d381ed65
Roman Norenberg <norromanv@yandex.ru>
2021-09-16 16:43:14 +0300

    Fix mistake

--- 3_selection_sort.js
+++ 3_selection_sort.js
@@ -1,11 +1,11 @@
-const arr = [3, 6, 1, -1, 21, 0, -23, 59, 5, 8, 101];
+const arr = [3, 6, 1, -1, 21, 0, -23, 59, 5, 8, 101, 6,7,8];
 let count = 0;
 
 function selectionSort(array) {
   for (let i = 0; i < array.length; i++) {
     let minIndex = i;
     for (let j = i + 1; j < array.length; j++) {
-      if (array[i] < array[minIndex]) {
+      if (array[j] < array[minIndex]) {
         minIndex = j;
       }
       count++;
